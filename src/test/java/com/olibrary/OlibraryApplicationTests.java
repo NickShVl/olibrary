3784415cbe926066f710cd148d44282199a87d93
NickShVl <Nekromimikon@mail.ru>
2025-03-09 17:12:07 +0400

    itit commit

---
+++ src/test/java/com/olibrary/OlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
